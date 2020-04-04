50652fbe425858d18653b185b35c8cacde8a1c46
Moriel Schottlender <moriel@gmail.com>
2020-04-04 13:35:45 -0700

    Adding deploy/ to gitignore

--- deploy/gas/api.js
+++
@@ -1,9 +0,0 @@
-function doGet() {
-  return HtmlService.createTemplateFromFile('index')
-    .evaluate()
-    .setSandboxMode(HtmlService.SandboxMode.IFRAME);
-}
-
-function getQuestions() {
-  return AppLib.spreadsheet.getQuestions();
-}
