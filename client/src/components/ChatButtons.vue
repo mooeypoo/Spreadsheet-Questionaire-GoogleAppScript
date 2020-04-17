c023cd57712ab396ed6db2ae8227d7311b7e9219
Moriel Schottlender <moriel@gmail.com>
2020-04-16 22:23:05 -0700

    Add ChatButtons

---
+++ client/src/components/ChatButtons.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="chat-buttons">
+    <b-button
+      pill
+      class="chat-buttons-button"
+      :href="slackLink"
+      target="_blank"
+      :variant="buttonData.variant"
+    >
+      <img src="https://img.icons8.com/officexs/24/000000/slack.png" />
+      {{buttonData.text.slack}}
+    </b-button>
+    <b-button
+      pill
+      class="chat-buttons-button"
+      :href="ircLink"
+      target="_blank"
+      :variant="buttonData.variant"
+    >
+      <small>IRC</small>
+      <!-- <img src="https://img.icons8.com/ios-glyphs/24/000000/chat.png" /> -->
+      {{buttonData.text.irc}}
+    </b-button>
+  </div>
+</template>
+
+<script>
+import Tools from "../services/Tools";
+export default {
+  name: "ChatButtons",
+  props: ["small"],
+  computed: {
+    slackLink: function() {
+      return Tools.isMobileOrTablet()
+        ? "slack://channel?team=T024KLHS4&id=C0120P3BKDZ"
+        : "https://wikimedia.slack.com/archives/C0120P3BKDZ";
+    },
+    ircLink: function() {
+      return Tools.isMobileOrTablet()
+        ? "https://webchat.freenode.net/?channels=wikimedia-staff"
+        : "https://www.irccloud.com/invite?channel=%23wikimedia-staff&hostname=irc.freenode.net&port=6697&ssl=1";
+    },
+    buttonData: function() {
+      if (this.small !== undefined) {
+        return {
+          variant: "outline-secondary",
+          text: { irc: "", slack: "" }
+        };
+      }
+
+      return {
+        variant: "dark",
+        text: {
+          irc: "#wikimedia-staff",
+          slack: "#distantsocializing"
+        }
+      };
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.chat-buttons {
+  small {
+    font-family: "Courier New", Courier, monospace;
+  }
+}
+</style>
